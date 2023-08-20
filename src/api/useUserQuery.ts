e0cdde0bfad80f579a334c1c0c3a33dafc0f5850
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-08-20 12:57:49 +0300

    [DRAFT]

--- src/api/useUserQuery.ts
+++ src/api/useUserQuery.ts
@@ -1,22 +1,31 @@
 import { useQuery } from '@tanstack/react-query'
 
+import { Action, useSearchContext } from '@/context'
+
 import { SuccessResponse } from './types'
 
 const BASE_URL = 'https://api.github.com/search/users'
 
 const useUserQuery = () => {
+  const [{ search }, dispatch] = useSearchContext()
+
   const handleFetch = async () => {
     const fullUrl = `${BASE_URL}?`
 
     return fetch(fullUrl).then(response =>
-      response.json().catch(error => error as Error),
+      response.json().catch(error => {
+        const { message } = error as Error
+
+        dispatch({ type: Action.SetError, payload: message })
+      }),
     )
   }
 
   const { data, isFetching, error } = useQuery<SuccessResponse, Error>({
     queryKey: ['users'],
     queryFn: handleFetch,
-    enabled: true,
+    enabled: !!search,
+    onSuccess: () => dispatch({ type: Action.DisableFirstSearch }),
   })
 
   return { data, isFetching, error }
--- src/components/SearchState/SearchState.tsx
+++ src/components/SearchState/SearchState.tsx
@@ -5,7 +5,7 @@ import * as S from './SearchState.style'
 const SearchState = () => {
   const [state] = useSearchContext()
 
-  return <S.SearchState>{JSON.stringify(state, null, 2)}</S.SearchState>
+  return <S.SearchState>{`SHOW ONLY IN DEV MODE:\n${JSON.stringify(state, null, 2)}`}</S.SearchState>
 }
 
 export default SearchState
